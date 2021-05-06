23741fa2e976270e2e7c9d14c0a65de39cd7327a
ryan <54sakuramiku@gmail.com>
2021-05-06 23:41:56 +0800

    d

--- backoffice/save.php
+++ backoffice/save.php
@@ -3,13 +3,19 @@
 include("config.inc.php");
 
 $account = test_input($_POST['account']);
-// $timestamp = $_POST['timestamp'];
-// $reason = $_POST['reason'];
-// $RE = $_POST['RE'];
-// $balance = $_POST['balance'];
-// $pay_method = $_POST['pay_method'];
+$timestamp = $_POST['timestamp'];
+$reason = $_POST['reason'];
+$RE = $_POST['RE'];
+$balance = $_POST['balance'];
+$pay_method = $_POST['pay_method'];
 
-echo $account;
+$sql = "INSERT INTO entries (`account`,`reason`,`RE`,`balance`,`pay_method`) VALUES ('$account','$reason',$RE,$balance,$pay_method)";
+if(mysqli_query($conn,$sql)){
+    echo "success insert data";
+}else{
+    echo "Error" . mysqli_error($conn);
+}
+
 
 function test_input($data){
     $data = trim($data);
